e856e7594e2d2fa49e86bf4c954d40b9cbee4926
Tom Yan <yanzhiyuan101@gmail.com>
2026-01-10 22:11:12 +0800

    Fix Tailwind v4 CSS configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
-
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import "./styles/globals.css";
 
 // Apply dark theme to the document
 document.documentElement.classList.add('dark');
